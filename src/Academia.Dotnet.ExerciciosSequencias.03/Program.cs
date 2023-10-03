d1a3a93c763731a7ef55146b5ed54157a4df16a8
PauloAlves8039 <pj38alves@gmail.com>
2023-10-03 19:31:08 -0300

    Aplicando solução para o exercício sequencial 03

---
+++ src/Academia.Dotnet.ExerciciosSequencias.03/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** Troca de Valores *****");
+
+        Console.Write("\nInforme o valor para A: ");
+        int valor1 = int.Parse(Console.ReadLine());
+
+        Console.Write("\nInforme o valor para B: ");
+        int valor2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"\nAntes da troca: A = {valor1}, B = {valor2}");
+
+        Console.WriteLine();
+
+        TrocaDeValores(valor1, valor2);
+
+        Console.ReadKey();
+    }
+
+    private static int TrocaDeValores(int numero1, int numero2) 
+    {
+        int A = numero1;
+        int B = numero2;
+
+        int temp = A;
+        A = B;
+        B = temp;
+
+        Console.WriteLine($"Depois da troca: A = {A}, B = {B}");
+
+        return A;
+    }
+}
