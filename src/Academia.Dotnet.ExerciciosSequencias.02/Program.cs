f7778081d793efe855c9c75f05276c1956d985be
PauloAlves8039 <pj38alves@gmail.com>
2023-10-03 16:49:11 -0300

    Aplicando solução para o exercício 02

---
+++ src/Academia.Dotnet.ExerciciosSequencias.02/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** Média Aritmética Entre 4 Valores *****");
+
+        /*
+         * 2) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre quatro números quaisquer.
+        */
+
+        int[] valores = new int[4];
+
+        Console.Write("\nInforme o primeiro valor: ");
+        int valor1 = int.Parse(Console.ReadLine());
+        valores[0] = valor1;
+
+        Console.Write("Informe o segundo valor: ");
+        int valor2 = int.Parse(Console.ReadLine());
+        valores[1] = valor2;
+
+        Console.Write("Informe o terceiro valor: ");
+        int valor3 = int.Parse(Console.ReadLine());
+        valores[2] = valor3;
+
+        Console.Write("Informe o quarto valor: ");
+        int valor4 = int.Parse(Console.ReadLine());
+        valores[3] = valor4;
+
+        var resultadoExercicio2 = CalcularMediaAritmeticaQuatroNumeros(valores);
+
+        Console.WriteLine($"\nA média aritmética entre [{valor1}, {valor2}, {valor3}, {valor4}] é: {resultadoExercicio2}");
+
+        Console.ReadKey();
+    }
+
+    private static int CalcularMediaAritmeticaQuatroNumeros(int[] numeros)
+    {
+        int soma = 0;
+
+        for (int i = 0; i < numeros.Length; i++) 
+        {
+            soma += numeros[i];
+        }
+
+        int media = (soma) / 4;
+
+        return media;
+    }
+}
