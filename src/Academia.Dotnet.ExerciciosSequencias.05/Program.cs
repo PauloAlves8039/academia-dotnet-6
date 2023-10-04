a259265e26f8e5be0f80a01ef828cd2330654439
PauloAlves8039 <pj38alves@gmail.com>
2023-10-03 21:20:38 -0300

    Criando solução para o exercício sequencial 05

---
+++ src/Academia.Dotnet.ExerciciosSequencias.05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 5) Escreva um programa em C# e no Visual Studio para calcular o consumo médio de um automóvel 
+         * (medido em Km/l), sendo que são conhecidos a distância total percorrida e o volume de combustível 
+         * consumido para percorrê-la (medido em l).
+        */
+
+        Console.WriteLine("***** Consumo Médio de Combustível *****");
+
+        Console.Write("Informe a distância total percorrida (em quilômetros): ");
+        double distanciaTotal = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe o volume de combustível consumido (em litros): ");
+        double volumeCombustivel = double.Parse(Console.ReadLine());
+
+        double resultadoExercicio5 = CalculandoConsumoDeCombustivel(distanciaTotal, volumeCombustivel);
+
+        Console.WriteLine("---------- Resultado ----------");
+
+        Console.WriteLine();
+        Console.WriteLine($"Consumo médio do combustível é: {resultadoExercicio5.ToString("F2", CultureInfo.InvariantCulture)} km/l");
+
+        Console.ReadKey();
+    }
+
+    private static double CalculandoConsumoDeCombustivel(double distancia, double volume) 
+    {
+        double consumoMedio = distancia / volume;
+        return consumoMedio;
+    }
+}
