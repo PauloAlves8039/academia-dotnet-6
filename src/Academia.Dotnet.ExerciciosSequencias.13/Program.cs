252bd1eb093807c9625b6a7fd37e547a7a11c1c6
PauloAlves8039 <pj38alves@gmail.com>
2023-10-04 16:11:52 -0300

    Implementando solução para o exercício sequencial 13

---
+++ src/Academia.Dotnet.ExerciciosSequencias.13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 13) Uma loja vende bicicletas com um acréscimo de 50% sobre o preço de custo. Ela paga a cada vendedor 2 salários mínimos mensais, mais uma comissão de 15% sobre o 
+         * preço de custo de cada bicicleta vendida. Desenvolva um algoritmo que leia o valor do salário mínimo, o preço de custo de cada bicicleta e o número de bicicletas 
+         * vendidas pelo vendedor, calcule e mostre: o salário do empregado
+        */
+
+        Console.WriteLine("***** Calcular Salário de Empregado");
+
+        Console.Write("Digite o valor do salário mínimo: ");
+        double salarioMinimo = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o preço de custo de cada bicicleta: ");
+        double precoCustoBicicleta = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o número de bicicletas vendidas pelo vendedor: ");
+        int numeroBicicletasVendidas = int.Parse(Console.ReadLine());
+
+        double resultadoExercicio13 = CalcularSalarioEmpregado(salarioMinimo, precoCustoBicicleta, numeroBicicletasVendidas);
+
+        Console.WriteLine($"O salário do empregado é: R$ {resultadoExercicio13.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        Console.ReadKey();
+    }
+
+    private static double CalcularSalarioEmpregado(double salarioMinimo, double precoCustoBicicleta, int numeroBicicletasVendidas)
+    {
+        double salarioEmpregado = 2 * salarioMinimo + (0.15 * precoCustoBicicleta * numeroBicicletasVendidas);
+        return salarioEmpregado;
+    }
+}
