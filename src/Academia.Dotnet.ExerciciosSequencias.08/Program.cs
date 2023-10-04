dfa4c06dcd432d371e8a46d11bae0fed3c113a16
PauloAlves8039 <pj38alves@gmail.com>
2023-10-04 14:53:38 -0300

    Criando solução para o exercício sequencial 08

---
+++ src/Academia.Dotnet.ExerciciosSequencias.08/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 8) Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. 
+         * A fórmula da conversão é F=(9*C+160)/5.
+        */
+
+        Console.WriteLine("***** Converter Celsius Para Fahrenheit *****");
+
+        Console.Write("Informe o valor Celsius: ");
+        double celsius = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("----- Resultado -----");
+
+        double resultadoExercicio1 = ConverterCelsiusParaFahrenheit(celsius);
+
+        Console.WriteLine($"A conversão para Fahrenheit é: {resultadoExercicio1.ToString("F2", CultureInfo.InvariantCulture)} °F");
+
+        Console.ReadKey();
+    }
+
+    private static double ConverterCelsiusParaFahrenheit(double celsius)
+    {
+        double fahrenheit = (9 * celsius + 160) / 5;
+        return fahrenheit;
+    }
+}
