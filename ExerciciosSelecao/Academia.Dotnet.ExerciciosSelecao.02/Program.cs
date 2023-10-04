b8a0adadd65f9738c2aec3b310e924d517712fbc
PauloAlves8039 <pj38alves@gmail.com>
2023-10-04 17:16:30 -0300

    Criando solução para o exercício seleção 02

---
+++ ExerciciosSelecao/Academia.Dotnet.ExerciciosSelecao.02/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 2 - Faça um algoritmo para ler um número e verificar se ele é par ou ímpar. 
+        */
+
+        Console.WriteLine("***** Verificar Par ou Ímpar *****");
+
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        VerificarPaiOuImpar(numero);
+
+        Console.ReadKey();
+    }
+
+    private static void VerificarPaiOuImpar(int numero) 
+    {
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"O número {numero} é par.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} é ímpar.");
+        }
+    }
+}
