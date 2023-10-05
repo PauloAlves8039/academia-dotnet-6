24305f01f66224deab37bc480e16687ad22e0601
PauloAlves8039 <pj38alves@gmail.com>
2023-10-05 13:28:17 -0300

    Criando solução para o exercício seleção 14

---
+++ ExerciciosSelecao/Academia.Dotnet.ExerciciosSelecao.14/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 14 - Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um 
+         * link de Internet (em Mbps). Em seguida, calcule e informe o tempo aproximado de download 
+         * do arquivo usando este link (em minutos).
+        */
+
+        Console.WriteLine("***** Calcular tempo de Download *****");
+
+        Console.Write("Digite o tamanho do arquivo em MB: ");
+        double tamanhoArquivoMB = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a velocidade da internet em Mbps: ");
+        double velocidadeInternetMbps = double.Parse(Console.ReadLine());
+
+        double tempoDownloadMinutos = CalcularTempoDownload(tamanhoArquivoMB, velocidadeInternetMbps);
+
+        Console.WriteLine("\n----- Resultado -----");
+
+        Console.WriteLine($"O tempo aproximado de download do arquivo é de {tempoDownloadMinutos:F2} minutos.");
+
+
+        Console.ReadKey();
+    }
+
+    private static double CalcularTempoDownload(double tamanhoArquivoMB, double velocidadeInternetMbps)
+    {
+        double velocidadeEmMBps = velocidadeInternetMbps * 0.125;
+        double tempoDownloadMinutos = tamanhoArquivoMB / velocidadeEmMBps / 60;
+        return tempoDownloadMinutos;
+    }
+}
