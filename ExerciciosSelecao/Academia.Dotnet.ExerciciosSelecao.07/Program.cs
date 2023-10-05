550aa00cb5613422c22afe11730c0ac75802a540
PauloAlves8039 <pj38alves@gmail.com>
2023-10-04 22:52:41 -0300

    Criando solução para o exercício seleção 07

---
+++ ExerciciosSelecao/Academia.Dotnet.ExerciciosSelecao.07/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 7 - Faça um programa em C# e no VS para receber uma frase qualquer e uma palavra de pesquisa.
+         * O programa deve avaliar se a palavra aparece na frase, informando o usuário via mensagem, como por 
+         * exemplo, A palavra encontra-se na frase   ou A palavra não se encontra na frase.
+        */
+
+        Console.WriteLine("***** Pesquisar Palavra *****");
+
+        Console.Write("Digite uma frase: ");
+        string frase = Console.ReadLine();
+
+        Console.Write("Digite uma palavra para pesquisar: ");
+        string palavra = Console.ReadLine();
+
+        Console.WriteLine("\n----- Resultado -----");
+
+        Console.WriteLine();
+
+        PesquisarPalavra(frase, palavra);
+
+        Console.ReadKey();
+    }
+
+    private static bool PesquisarPalavra(string frase, string palavra) 
+    {
+        bool palavraRecebida = frase.Contains(palavra);
+
+        if (palavraRecebida)
+        {
+            Console.WriteLine($"A palavra '{palavra}' encontra-se na frase '{frase}'");
+        }
+        else
+        {
+            Console.WriteLine($"A palavra '{palavra}' não se encontra na frase '{frase}'");
+        }
+
+        return palavraRecebida;
+    } 
+}
