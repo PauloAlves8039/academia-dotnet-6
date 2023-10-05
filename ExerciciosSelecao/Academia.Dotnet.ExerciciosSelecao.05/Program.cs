96e09a564d4d953086dc0c8fdd8da32cb91b940d
PauloAlves8039 <pj38alves@gmail.com>
2023-10-04 21:20:26 -0300

    Criando solução para o exercício seleção 05

---
+++ ExerciciosSelecao/Academia.Dotnet.ExerciciosSelecao.05/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("***** Média de Duas Notas *****");
+
+        Console.Write("Informe a primeira nota: ");
+        double nota1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Informe a segunda nota: ");
+        double nota2 = double.Parse(Console.ReadLine());
+
+        double media = CalcularMedia(nota1, nota2);
+
+        if (media > 7.0)
+        {
+            Console.Write("Informe a nota do exame: ");
+            double notaExame = double.Parse(Console.ReadLine());
+
+            double mediaFinal = CalcularMediaFinal(media, notaExame);
+
+            if (mediaFinal >= 5.0)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Aprovado");
+        }
+
+        Console.ReadKey();
+    }
+
+    private static double CalcularMedia(double nota1, double nota2)
+    {
+        return (nota1 + nota2) / 2;
+    }
+
+    private static double CalcularMediaFinal(double media, double notaExame)
+    {
+        return (media + notaExame) / 2;
+    }
+}
