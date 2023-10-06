932f929ef45fab3c6579dc72b0e2eaf180049d23
PauloAlves8039 <pj38alves@gmail.com>
2023-10-06 12:29:00 -0300

    Desenvolvendo solução para o exercício repetição 04

---
+++ ExerciciosRepeticao/Academia.Dotnet.ExerciciosRepeticao.04/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 4. Escreva um algoritmo que leia dois valores pelo teclado, x e y, e em seguida calcule a potência de x
+         * elevado na y sem utilizar a função pow.
+        */
+
+        Console.WriteLine("***** Calcular Potência *****");
+
+        Console.Write("Digite o valor de x: ");
+        double x = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de y: ");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n----- Resultado -----");
+
+        double resultado = CalcularPotencia(x, y);
+        Console.Write($"{x} elevado a {y} é igual a {resultado}");
+
+        Console.ReadKey();
+    }
+
+    private static double CalcularPotencia(double x, int y)
+    {
+        double resultado = 1.0;
+
+        for (int i = 1; i <= y; i++)
+        {
+            resultado *= x;
+        }
+
+        return resultado;
+    }
+}
