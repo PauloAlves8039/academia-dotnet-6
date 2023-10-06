8c0fe25e4098709e588d0200674f6d531a354e52
PauloAlves8039 <pj38alves@gmail.com>
2023-10-06 14:48:47 -0300

    Criando solução para o exercício repetição 07

---
+++ ExerciciosRepeticao/Academia.Dotnet.ExerciciosRepeticao.07/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numerosPares = 0;
+        int numerosImpares = 0;
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.Write($"Digite o {i}º número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                numerosPares++;
+            }
+            else
+            {
+                numerosImpares++;
+            }
+        }
+
+        Console.WriteLine($"Quantidade de números pares: {numerosPares}");
+        Console.WriteLine($"Quantidade de números ímpares: {numerosImpares}");
+
+
+        Console.ReadKey();
+    }
+}
