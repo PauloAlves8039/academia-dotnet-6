f2320e27d0fa6ac19ba6656a08d25f06b2910078
PauloAlves8039 <pj38alves@gmail.com>
2023-10-06 18:31:06 -0300

    Refatorando o exercício repetição pt2 04

--- ExerciciosRepeticaoPT2/Academia.Dotnet.ExerciciosRepeticaoPT2.04/Program.cs
+++ ExerciciosRepeticaoPT2/Academia.Dotnet.ExerciciosRepeticaoPT2.04/Program.cs
@@ -1,71 +1,61 @@
 ﻿internal class Program
 {
-    private static int votosJoao = 0;
-    private static int votosZeca = 0;
-    private static int votosBranco = 0;
-    private static int votosNulo = 0;
-    private static int totalVotos = 0;
-
     private static void Main(string[] args)
     {
-        InstrucoesDaVotacao();
+        /*
+         * 4. Em uma cidadezinha do interior há dois candidatos para prefeito (JOAO e ZECA). 
+         * Você foi contratado para fazer um programa em VS que conte os votos dos candidatos. 
+         * Cada vez que o usuário digitar JOAO, você deve somar um ao seu número de votos. O mesmo para ZECA. 
+         * Você também pode aceitar votos em branco (palavra BRANCO). O programa em VS termina quando o usuário 
+         * escrever FIM. Ao final devem ser mostradas as quantidades de votos para cada um dos candidatos e o número 
+         * de votos em branco. 
+        */
+
+        int votosJoao = 0;
+        int votosZeca = 0;
+        int votosBranco = 0;
+
+        Console.WriteLine("Digite 'JOAO' para votar em João.");
+        Console.WriteLine("Digite 'ZECA' para votar em Zeca.");
+        Console.WriteLine("Digite 'BRANCO' para votar em branco.");
+        Console.WriteLine("Digite 'FIM' para encerrar.");
 
         while (true)
         {
             string voto = Console.ReadLine().ToUpper();
 
             if (voto == "FIM")
-            {
                 break;
-            }
 
-            totalVotos++;
-            ProcessamentoDoVoto(voto);
+            if (voto == "JOAO")
+            {
+                votosJoao++;
+            }
+            else if (voto == "ZECA")
+            {
+                votosZeca++;
+            }
+            else if (voto == "BRANCO")
+            {
+                votosBranco++;
+            }
+            else
+            {
+                Console.WriteLine("Voto inválido. Tente novamente.");
+            }
         }
 
-        ApresentacaoDosResultado();
+        ApresentacaoDosResultado(votosJoao, votosZeca, votosBranco);
+
 
         Console.ReadKey();
     }
 
-    private static void InstrucoesDaVotacao()
-    {
-        Console.WriteLine("Digite 'JOAO' para votar em João.");
-        Console.WriteLine("Digite 'ZECA' para votar em Zeca.");
-        Console.WriteLine("Digite 'BRANCO' para votar em branco.");
-        Console.WriteLine("Digite 'NULO' para votar nulo.");
-        Console.WriteLine("Digite 'FIM' para encerrar.");
-    }
-
-    private static void ProcessamentoDoVoto(string voto)
-    {
-        switch (voto)
-        {
-            case "JOAO":
-                votosJoao++;
-                break;
-            case "ZECA":
-                votosZeca++;
-                break;
-            case "BRANCO":
-                votosBranco++;
-                break;
-            case "NULO":
-                votosNulo++;
-                break;
-            default:
-                Console.WriteLine("Voto inválido! Tente novamente.");
-                break;
-        }
-    }
-
-    private static void ApresentacaoDosResultado()
+    private static void ApresentacaoDosResultado(int votosJoao, int votosZeca, int votosBranco)
     {
         Console.WriteLine("\n----- Resultado -----");
         Console.WriteLine($"Total de votos para JOAO: {votosJoao}");
         Console.WriteLine($"Total de votos para ZECA: {votosZeca}");
         Console.WriteLine($"Total de votos em branco: {votosBranco}");
-        Console.WriteLine($"Total de votos nulos: {votosNulo}");
-        Console.WriteLine($"Total de pessoas que votaram: {totalVotos}");
     }
 }
