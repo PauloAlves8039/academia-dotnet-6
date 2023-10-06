8892d1d8c3bac8996be5ba504a929cbb3014552a
PauloAlves8039 <pj38alves@gmail.com>
2023-10-06 12:02:03 -0300

    Implementando solução para o exercício repetição 03

---
+++ ExerciciosRepeticao/Academia.Dotnet.ExerciciosRepeticao.03/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 3. Escreva um algoritmo que leia pelo teclado um valor e então calcule a tabuada de 1 a 10 de um número
+         * x lido utilizando laços de repetição, e mostre na tela.
+        */
+
+        Console.Write("Digite um número para calcular a tabuada: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n----- Resultado -----");
+
+        ExibirTabuadaDeSoma(numero);
+
+        Console.ReadKey();
+    }
+
+    private static void ExibirTabuadaDeSoma(int numero)
+    {
+        Console.WriteLine($"Tabuada de {numero} (soma): ");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int resultado = numero + i;
+            Console.WriteLine($"{numero} + {i} = {resultado}");
+        }
+    }
+}
