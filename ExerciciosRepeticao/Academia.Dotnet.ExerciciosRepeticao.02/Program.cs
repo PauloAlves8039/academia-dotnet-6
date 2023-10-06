be668f3771a24d679731695643008b552a9615f8
PauloAlves8039 <pj38alves@gmail.com>
2023-10-06 11:38:50 -0300

    Aplicando solução para o exercício repetição 02

---
+++ ExerciciosRepeticao/Academia.Dotnet.ExerciciosRepeticao.02/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+         * 2. Faça um algoritmo que leia 10 números pelo teclado, e que no final mostre o maior deles. Não usar vetor.
+        */
+
+        int maiorNumero = int.MinValue;
+        int numero = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Digite o " + i + "º número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            VerificarMaiorNumero(numero, maiorNumero);
+        }
+
+        Console.WriteLine("\n----- Resultado -----");
+
+        int resultadoExercicio2 = VerificarMaiorNumero(numero, maiorNumero);
+        Console.WriteLine($"O maior número é: {resultadoExercicio2}");
+
+        Console.ReadKey();
+    }
+
+    private static int VerificarMaiorNumero(int numero, int maiorNumero) 
+    {
+        int resultado = maiorNumero;
+
+        if (numero > resultado) 
+        {
+            resultado = numero;
+        }
+
+        return resultado;
+    }
+}
