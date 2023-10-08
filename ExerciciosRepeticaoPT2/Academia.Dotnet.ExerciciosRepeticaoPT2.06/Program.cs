fcb86079df22d88863229c27ee9d006dcf6a925f
PauloAlves8039 <pj38alves@gmail.com>
2023-10-08 15:11:31 -0300

    Adicionando solução para o exercício repetição pt2 06

---
+++ ExerciciosRepeticaoPT2/Academia.Dotnet.ExerciciosRepeticaoPT2.06/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int idade;
+        do
+        {
+            Console.Write("Digite a idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+        } while (!VerificarIdade(idade));
+
+        Console.ReadKey();
+    }
+
+    private static bool VerificarIdade(int idade) 
+    {
+        if (idade <= 0)
+        {
+            Console.WriteLine("Idade inválida - informe uma idade acima de zero.");
+            return false;
+        }
+        else 
+        {
+            Console.WriteLine($"Idade digitada: {idade}");
+            return true;
+        }
+    }
+}
